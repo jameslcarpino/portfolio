a52ca3effb6c1aef5d9b401331000430bf1216c9
Pinefoot <jameslcarpino@gmail.com>
2020-09-24 11:32:13 -0700

    basic style structure

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,31 +1,25 @@
 import React from "react";
-import Board from "./Boards";
-import Card from "./Card";
+import Board from "./Boards_Cards/Boards";
+import Card from "./Boards_Cards/Card";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 export default function Home() {
   return (
-    <div className="flexbox">
-      <div>
-        <h2> Hi!</h2>
-        <h2> My name is James Carpino</h2>
-        <p>I'm a full stack web developer</p>
-        <p>
-          {" "}
-          Want to learn more? Drop one of these icons into the rabbit hole.
-        </p>
+    <>
+      <div className="nav-bar" style={{ textAlign: "right" }}>
+        <p>placeholder text for nav bar component</p>
       </div>
-      <div>
-        <Board id="board-1" className="board">
-          <Card id="card-1" classname="card" draggable="true">
-            <p>TEST DRAG DROP</p>
-          </Card>
-        </Board>
-
-        <Board id="board-2" className="board">
-          <Card id="card-2" className="card" draggable="true">
-            <p>TEST 2 CARD</p>
-          </Card>
-        </Board>
+      <div className="home-container">
+        <div>
+          <h3 className="intro-text">Hello there!</h3>
+          <h3 className="intro-text">My name</h3>
+          <h3 className="intro-text">Is James</h3>
+        </div>
+        <div>
+          <p className="intro-p">I'm a full stack web developer.</p>
+          <p className="intro-learn">Want to learn more?</p>
+          <p className="intro-learn">Drag an icon into the rabbit hole!</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
