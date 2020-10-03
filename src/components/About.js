f8a444819deffd15b4c49ebf8f2ed16111b16b12
Pinefoot <jameslcarpino@gmail.com>
2020-10-03 16:24:11 -0700

    working on About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import james from "./james.jpg";
+
 export default function About() {
   return (
     <>
       <Card>
-        <Card.Title>About James</Card.Title>
-
-        <p>
-          As you alreayd know, my name is James. Here are some quick things you
-          may not know about me!
-        </p>
-        <p>I am a human.</p>
-        <p>I am in love with learning anythign and everything I can</p>
-        <p>I will believe any and every ghost story someone tells me.</p>
+        <Modal.Header>
+          <Card.Title>About James</Card.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Image src={james} alt="james" rounded />
+          <Card.Text>
+            As you alreayd know, my name is James. Here are some quick things
+            you may not know about me!
+          </Card.Text>
+          <Card.Text>I am a human.</Card.Text>
+          <Card.Text>
+            I am in love with learning anythign and everything I can
+          </Card.Text>
+          <Card.Text>
+            I will believe any and every ghost story someone tells me.
+          </Card.Text>
+        </Modal.Body>
       </Card>
     </>
   );
---
+++ src/components/James.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { james } from "./james.jpg";
+export default function James() {
+  return <img src={james} alt="james"></img>;
+}
