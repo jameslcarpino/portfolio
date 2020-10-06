fc6880446ddd03f51f439649cd585f2781c1a048
Pinefoot <jameslcarpino@gmail.com>
2020-10-05 22:04:56 -0700

    finished up last of the language on projects page. all thats left is some media breakpoints to handle font sizing.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,45 +1,93 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import CardDeck from "react-bootstrap/CardDeck";
+import CardGroup from "react-bootstrap/CardGroup";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import ModalBody from "react-bootstrap/ModalBody";
 import { projectData } from "./projectData";
 
-export default function Projects(props) {
-  let { handleCancel } = props;
+export default function Projects() {
   return (
     <>
       {projectData.map((item) => (
         <ModalBody>
-          <Card style={{ textAlign: "center" }}>
-            <Image size="sm" variant="top" fluid src={item.image} />
-            <Card.Body>
-              <div>
-                <Card.Subtitle>PROJECT NAME</Card.Subtitle>
-                <Card.Title>{item.name}</Card.Title>
-                <Card.Text>{item.description}</Card.Text>
-                <Card.Subtitle>CONTRIBUTIONS</Card.Subtitle>
-                {item.roles.map((tasks) => (
-                  <Card.Text>{tasks.role}</Card.Text>
-                ))}
-                <Card.Subtitle>TECHNOLOGY USED</Card.Subtitle>
-                <Card.Text>
-                  {item.technology.frontend.length > 0
-                    ? `Front End: ${item.technology.frontend}`
-                    : null}
-                </Card.Text>
-                <Card.Text>
-                  {item.technology.backend.length > 0
-                    ? `Back End: ${item.technology.backend}`
-                    : null}
-                </Card.Text>
-              </div>
+          <CardGroup>
+            <Card style={{ textAlign: "center" }}>
+              <Image
+                size="sm"
+                variant="top"
+                fluid
+                src={item.image}
+                style={{ textAlign: "center" }}
+              />
+              <Card.Body style={{ textAlign: "justify" }}>
+                <div>
+                  <Card.Title style={{ fontSize: "2.1rem" }}>
+                    {item.name}
+                  </Card.Title>
+                  <Card.Text>
+                    <ul>
+                      <p>{item.description}</p>
+                    </ul>
+                  </Card.Text>
+                  <Card.Title>CONTRIBUTIONS</Card.Title>
+                  <div style={{ textAlign: "justify" }}>
+                    {item.roles.map((tasks) => (
+                      <Card.Text style={{ fontSize: "14px" }}>
+                        <ul>{tasks.role}</ul>
+                      </Card.Text>
+                    ))}
+                  </div>
+                  <Card.Title>TECHNOLOGY USED</Card.Title>
 
-              <Button variant="dark">View Project</Button>
-              <Button variant="dark">View Github</Button>
-            </Card.Body>
-          </Card>
+                  <Card.Text>
+                    <ul>
+                      {" "}
+                      {item.technology.frontend.length > 0 ? (
+                        <>
+                          <Card.Subtitle style={{ fontStyle: "italic" }}>
+                            Front End
+                          </Card.Subtitle>
+                          {item.technology.frontend}
+                        </>
+                      ) : null}
+                    </ul>
+                  </Card.Text>
+
+                  <Card.Text>
+                    <ul>
+                      {item.technology.backend.length > 0 ? (
+                        <>
+                          <Card.Subtitle style={{ fontStyle: "italic" }}>
+                            Back End
+                          </Card.Subtitle>
+                          {item.technology.backend}
+                        </>
+                      ) : null}
+                    </ul>
+                  </Card.Text>
+                </div>
+
+                <Button
+                  variant="dark"
+                  style={{ marginRight: "2%" }}
+                  onClick={() => {
+                    window.location.href = `${item.website}`;
+                  }}
+                >
+                  View Project
+                </Button>
+                <Button
+                  variant="dark"
+                  onClick={() => {
+                    window.open(`${item.github}`);
+                  }}
+                >
+                  View Github
+                </Button>
+              </Card.Body>
+            </Card>
+          </CardGroup>
         </ModalBody>
       ))}
     </>
